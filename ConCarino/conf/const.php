5012ec097f79210f1ee135245fc7650734209657
YUKI <ykojima12@gmail.com>
2017-08-31 07:32:39 +0000

    backup_ConCarino_2

--- ConCarino/conf/const.php
+++ ConCarino/conf/const.php
@@ -2,7 +2,7 @@
 
 // データベースの接続情報
 define('DB_USER', 'kjmyk12'); // MySQLのユーザー名
-define('DB_PASSWD', ''); // MySQLのパスワード
+define('DB_PASSWD', 'password'); // MySQLのパスワード
 define('DSN', 'mysql:dbname=camp;host=localhost;charset=utf8'); // データベースのDSN情報
 
 define('HTML_CHARACTER_SET', 'UTF-8'); // HTML文字エンコーディング
--- PHP/25/bbs/conf/setting.php
+++ PHP/25/bbs/conf/setting.php
@@ -2,7 +2,7 @@
 
 // データベースの接続情報
 define('DB_USER', 'kjmyk12'); //MySQLのユーザー名
-define('DB_PASSWD', ''); //MySQLのパスワード
+define('DB_PASSWD', 'password'); //MySQLのパスワード
 define('DSN', 'mysql:dbname=camp;host=localhost;charset=utf8'); //データベースのDSN情報
 //define('DSN', 'mysql:dbname=camp;host=localhost;charset=utf8'); // mysqlの後は、コロンです。by ueda
 
